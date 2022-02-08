ed12a8fda8418158d9fa339605a70ac9fbf5f613
FTC-5703 <98932332+FTC-5703@users.noreply.github.com>
2022-02-07 23:22:50 -0600

    Uploaded Arcade Drive teleop
    Changed up button mappings a bit; moved away from the bumpers since they're sus

--- TeleOp_Comp_2021.java
+++ TeleOp_Comp_2021.java
@@ -9,14 +9,14 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 @TeleOp(name = "TeleOp_Comp_2021", group = "")
-public class TeleOP_Comp_2021 extends LinearOpMode  {
+class TeleOP_Comp_2021 extends LinearOpMode  {
 
    //Define Motors and Touchsenor
   private DcMotor motorRR;
   private DcMotor motorRF;
   private DcMotor motorLR;
   private DcMotor motorLF;
-  //private REVTouchSensor Grabberlimit;
+  private TouchSensor Grabberlimit;
   private DcMotor LiftMotor;
   private DcMotor Duckmotor;
   private CRServo orgServo;
@@ -32,114 +32,54 @@ public class TeleOP_Comp_2021 extends LinearOpMode  {
     orgServo = hardwareMap.crservo.get("orgServo");
     LiftMotor = hardwareMap.dcMotor.get("LiftMotor");
     Duckmotor = hardwareMap.dcMotor.get("Duckmotor");
-    //Grabberlimit = hardwareMap.touchSensor.get("Grabberlimit");
+    Grabberlimit = hardwareMap.touchSensor.get("Grabberlimit");
        motorLR.setDirection(DcMotor.Direction.REVERSE);
        motorLF.setDirection(DcMotor.Direction.REVERSE);
     
     
     waitForStart();
-    if (opModeIsActive()) {
       while (opModeIsActive()) {
 
-    
-          //Driving
-          motorRR.setPower(gamepad1.right_stick_y);
-          motorRF.setPower(gamepad1.right_stick_y);
-          motorLR.setPower(gamepad1.left_stick_y);
-          motorLF.setPower(gamepad1.left_stick_y);
-          
-          // lift motor 
-          LiftMotor.setPower((double) gamepad1.right_trigger);
-          LiftMotor.setPower(((double) gamepad1.left_trigger)*-1);
-          
+          //Improved driving
+          float left = -gamepad1.left_stick_y  + gamepad1.left_stick_x;
+          float right = -gamepad1.left_stick_y  - gamepad1.left_stick_x;
+          motorRR.setPower(right); motorRF.setPower(right); //right motors
+          motorLR.setPower(left);  motorLF.setPower(left);  //left motors
+
+          //telemetry nonsense
+          telemetry.addData("Encoder Ticks", motorRR.getCurrentPosition());
+          telemetry.addData("Servo Power: ", orgServo.getPower());
+
+          // lift motor
+          if(gamepad1.dpad_up){
+              LiftMotor.setPower(1);
+          } else if(gamepad1.dpad_down){
+              LiftMotor.setPower(-1);
+          } else{
+              LiftMotor.setPower(0);
+          }
+
           // Duck motor
-          if(gamepad1.right_bumper){
-            Duckmotor.setPower(0.7);
-          }else{
-            Duckmotor.setPower(0);
+          if (gamepad1.right_trigger > .1) {
+              Duckmotor.setPower(0.7);
+          } else if (gamepad1.left_trigger > .1) {
+              Duckmotor.setPower(-0.5);
+          } else {
+              Duckmotor.setPower(0);
           }
-          
-          if(gamepad1.left_bumper){
-            Duckmotor.setPower(-0.5);
-          }else{
-            Duckmotor.setPower(0);
-          }
-          
-        
-            }
-          
-          
-       if (gamepad1.y){
-             orgServo.setPower(-0.5);
-       }else{
-         orgServo.setPower(0);
-         
-       }
-          
-          if(gamepad1.x){
-              orgServo.setPower(0.5);
-          }else{    
+
+          if (gamepad1.y) {
+              orgServo.setPower(-0.5);
+          } else {
               orgServo.setPower(0);
           }
-      
-      
-      
-      
-      /*
-          //driving 
-        if (gamepad1.dpad_up) {
-          motorRR.setPower(1);
-          motorRF.setPower(1);
-          motorLR.setPower(-1);
-          motorLF.setPower(-1);
-          
-          
-        } else {
-          motorRR.setPower(0);
-          motorRF.setPower(0);
-        }
-       
-       if (gamepad1.dpad_down){
-         motorLR.setPower(1);
-         motorLF.setPower(1);
-       } else {
-         motorLR.setPower(0);
-         motorLF.setPower(0);
-       }
-       if (gamepad1.dpad_left){
-         motorLR.setPower(1);
-         motorLF.setPower(1);
-         motorRR.setPower(1);
-         motorRF.setPower(1);
-       } else {
-         motorLR.setPower(0);
-         motorLF.setPower(0);
-         motorRR.setPower(0);
-         motorRF.setPower(0);
-       }
-       
-      if (gamepad1.dpad_right){
-         motorLR.setPower(-1);
-         motorLF.setPower(-1);
-         motorRR.setPower(-1);
-         motorRF.setPower(-1);
-      } else {
-         motorLR.setPower(0);
-         motorLF.setPower(0);
-         motorRR.setPower(0);
-         motorRF.setPower(0);
-       }
-      
-       
-        
-         if (Grabberlimit.getState()) {
-            orgServo.setPower(0);
-        }else{
-            orgServo.setPower(0.5);
-         }
-         
-        */ 
-  
-    }
-  }
+
+          if (!(Grabberlimit.isPressed()) && gamepad1.x) {
+              orgServo.setPower(0.5);
+          } else {
+              orgServo.setPower(0);
+          }
+
+      }
+   }
 }
